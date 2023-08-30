bfbce85f0248d79d013b76b98efc875acbe2971d
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-08-30 16:02:27 -0300

    Implementa construtor e parametros requeridos para a instância de um novo objeto

---
+++ src/Bicicleta.java
@@ -0,0 +1,14 @@
+public class Bicicleta {
+    String nome;
+    String descricao;
+    double valor;
+
+    public Bicicleta(String nome, String descricao, double valor){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Bicicleta bicicleta = new Bicicleta(
+                "Oggi Hacker Pro Aro 29", "Bike top", 1799.99);
     }
 }
 
