8f70b72e43e2060b91d45b2435ee9f0781d845e0
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-08-30 16:14:57 -0300

    Define um atributo static para a classe Bicicleta

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bicicleta {
     String nome;
     String descricao;
     double valor;
+    static List<Bicicleta> listaBicicletas = new ArrayList<>();
 
     public Bicicleta(String nome, String descricao, double valor){
         this.nome = nome;
         this.descricao = descricao;
         this.valor = valor;
+        listaBicicletas.add(this);
     }
 }
+
+
 
 
 
