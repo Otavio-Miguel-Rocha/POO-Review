d19e521f80215aca0970bd120950ebe85e2e1660
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-08-30 16:36:28 -0300

    Implementa método static para encontrar um bicicleta

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -2,16 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bicicleta {
+    int id;
     String nome;
     String descricao;
     double valor;
     static List<Bicicleta> listaBicicletas = new ArrayList<>();
+    static int contadorId = 0;
 
     public Bicicleta(String nome, String descricao, double valor){
+        contadorId++;
+        this.id = contadorId;
         this.nome = nome;
         this.descricao = descricao;
         this.valor = valor;
         listaBicicletas.add(this);
+    }
+
+    public static Bicicleta encontrarBicicleta(int id){
+        for ( Bicicleta bicicleta : listaBicicletas ) {
+            if(bicicleta.id == id){
+                return bicicleta;
+            }
+        }
+        return null;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Bicicleta bicicleta = new Bicicleta(
-                "Oggi Hacker Pro Aro 29", "Bike top", 1799.99);
+        dadosIniciais();
+        //Vamos imaginar que no sistema, estamos pesquisando por uma bike
+        //vamos pesquisar ela pelo código de barra(ID)
+        //Que nesse exemplo é 3
+        Bicicleta bicicleta = Bicicleta.encontrarBicicleta(3);
+
+        //A saída será Monark
+        System.out.println(bicicleta.nome);
+    }
+
+    public static void dadosIniciais(){
+       new Bicicleta("Oggi Hacker Pro Aro 29", "Bike top", 1799.99);
+
+        new Bicicleta("Soul Pro Rider", "Bike High Level", 2799.99);
+
+        new Bicicleta("Monark", "Bike old school", 799.99);
+
+        new Bicicleta("Sense Gravel", "Bike expert", 5399.99);
     }
 }
 
