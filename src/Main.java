f98a6bb59506c84f2842dd1414ee5362b383470b
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-08-30 14:48:24 -0300

    Exemplo de manipulação com Matriz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //Criação de uma matriz de uma loja de bicicletas
+        String [][] bicicletas = new String[5][5];
 
+        //Definição do nome da bicicleta
+        bicicletas[4][0] = "Oggi Hacker Pro Aro 29";
+
+        //Manipulando a descrição da 5ª bicicleta da loja através do nome dela
+        for (int i = 0; i < bicicletas.length; i++) {
+            if(bicicletas[i][0] != null){
+                if(bicicletas[i][0].equals("Oggi Hacker Pro Aro 29")){
+                    bicicletas[i][1] = "Bicicleta Top de Linha";
+                }
+            }
+        }
     }
 }
 
