646721c1ca1afe5e98133eca79f0e61360e780bb
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-09-01 09:30:33 -0300

    Implemente o super na classe Bicicleta

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -5,8 +5,11 @@ public class Bicicleta extends Produto{
     private double tamanhoAro;
     private double tamanhoQuadro;
 
-    public Bicicleta(String nome, String descricao, double valor){
-
+    public Bicicleta(String nome, String descricao, double valor,
+                     double tamanhoAro, double tamanhoQuadro){
+        super(nome, descricao, valor);
+        this.tamanhoAro = tamanhoAro;
+        this.tamanhoQuadro = tamanhoQuadro;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         dadosIniciais();
-
-        Vendedor vendedor = new Vendedor("Marcos");
-
-        //Nome antes da alteração
-        System.out.println(vendedor.getNome());
-
-        //Alteração do nome, para nome e sobrenome
-        vendedor.setNome("Marcos Rocha");
-
-        //Nome após a alteração
-        System.out.println(vendedor.getNome());
     }
 
     public static void dadosIniciais() {
-        new Bicicleta("Oggi Hacker Pro Aro 29", "Bike top", 1799.99);
-        new Bicicleta("Soul Pro Rider", "Bike High Level", 2799.99);
-        new Bicicleta("Monark", "Bike old school", 799.99);
-        new Bicicleta("Sense Gravel", "Bike expert", 5399.99);
+        new Bicicleta("Oggi Hacker Pro Aro 29", "Bike top", 1799.99, 29, 15.5);
+        new Bicicleta("Soul Pro Rider", "Bike High Level", 2799.99, 29, 17);
+        new Bicicleta("Monark", "Bike old school", 799.99, 29, 15);
+        new Bicicleta("Sense Gravel", "Bike expert", 5399.99, 29, 17);
+
+        new Vendedor("Marcos");
+        new Vendedor("Gabriel");
     }
 }
 
